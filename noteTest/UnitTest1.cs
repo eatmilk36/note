0a3d9baf26f633ea40051da326733ed26fbc700b
jeter <a0958672769@gmail.com>
2024-02-27 16:27:05 +0800

    加入專案檔案。

---
+++ noteTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace noteTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
